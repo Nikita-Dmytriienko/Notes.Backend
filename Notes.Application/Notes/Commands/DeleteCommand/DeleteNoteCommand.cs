62145770237842ee2e2081492c1d8a5238b4e922
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-12-15 21:11:46 +0200

    feat:add DeleteNoteCommandHandler.cs

--- Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommand.cs
+++ Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommand.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MediatR;
 
 namespace Notes.Application.Notes.Commands.DeleteCommand
 {
-    public class DeleteNoteCommand
+    public class DeleteNoteCommand :IRequest
     {
         public Guid UserId { get; set; }
         public Guid Id { get; set; }
