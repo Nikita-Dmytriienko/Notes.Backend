982c71fa970cdf945e3acd8144ca588735639cf7
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-12-15 15:10:24 +0200

    feat:add UpdateNoteCommandHandler.cs

---
+++ Notes.Application/Notes/Commands/UpdateNote/UpdateNoteCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Notes.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Notes.Application.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommandHandler : IRequestHandler<UpdateNoteCommand>
+    {
+        private readonly INotesDbContext dbContext;
+
+        public UpdateNoteCommandHandler(INotesDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Unit> Handle(UpdateNoteCommand request,
+            CancellationToken cancellationToken)
+        {
+            var entity =
+                await _dbContext.Notes.FirstOrDefaultAsync(
+            return Unit.Value;
+        }
+
+    }
+}
