33154495ab6970eecc1472dfafb5a4a5e22e6641
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-12-16 14:56:23 +0200

    feat:add Mapping for our applicataion

--- Notes.Application/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
+++ Notes.Application/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using Notes.Application.Common.Mappings;
 using Notes.Domain;
 
 namespace Notes.Application.Notes.Queries.GetNoteDetails
 {
-    public class NoteDetailsVm :IMapWith<Note>
+    public class NoteDetailsVm : IMapWith<Note>
     {
+        public Guid Id { get; set; }
+        public required string Details { get; set; }
+        public required string Title { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Note, NoteDetailsVm>()
+                .ForMember(noteVm => noteVm.Title, opt => opt.MapFrom(note => note.Title))
+                .ForMember(noteVm => noteVm.Details, opt => opt.MapFrom(note => note.Details))
+                .ForMember(noteVm => noteVm.Id, opt => opt.MapFrom(note => note.Id))
+                .ForMember(noteVm => noteVm.CreationDate, opt => opt.MapFrom(note => note.CreationDate))
+                .ForMember(noteVm => noteVm.EditDate, opt => opt.MapFrom(note => note.EditDate));
+
+        }
+
 
     }
 }
