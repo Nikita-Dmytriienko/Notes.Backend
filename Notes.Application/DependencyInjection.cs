a8b3d526098ecce48ebb356633ecc92c45092e88
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-12-16 16:51:16 +0200

    feat:add DependencyInjection.cs for dependencies

---
+++ Notes.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MediatR;
+
+namespace Notes.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection
+            services)
+        {
+            return services;
+        }
+    }
+}
