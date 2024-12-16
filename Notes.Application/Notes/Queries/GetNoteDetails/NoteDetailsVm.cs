5156a6794bd2ece5e3b9252202c036415fd5e473
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-12-16 14:44:40 +0200

    feat:add folder Queries with GetNoteDetailsQuery.cs and NoteDetailsVm.cs.

---
+++ Notes.Application/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notes.Application.Common.Mappings;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class NoteDetailsVm :IMapWith<Note>
+    {
+
+    }
+}
