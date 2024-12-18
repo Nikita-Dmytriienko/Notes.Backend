c6788b3506acb3ad9d000207cc67a8946f166ae7
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-12-18 13:45:02 +0200

    feat(api): add NoteController with endpoints for fetching notes

---
+++ Notes.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MediatR;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using Notes.Application.Notes.Queries.GetNoteList;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+
+namespace Notes.WebApi.Controllers
+{
+    public class NoteController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<NoteListVm>> GetAll()
+        {
+            var query = new GetNoteListQuery
+            {
+                UserId = UserId
+            };
+
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NoteDetailsVm>>Get(Guid id)
+        {
+            var query = new GetNoteDetailsQuery
+            {
+                UserId = UserId,
+                Id = id
+            };
+            var vm=await Mediator.Send(query);
+            return Ok(vm);
+        }
+    }
+}
