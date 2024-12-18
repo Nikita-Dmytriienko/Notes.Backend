307dbe28654244c41018ef733c70956f19192c21
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-12-18 14:23:39 +0200

    feat(api): add Update endpoint for note updates refactor: improve mapping logic in controllers

--- Notes.WebApi/Controllers/NoteController.cs
+++ Notes.WebApi/Controllers/NoteController.cs
@@ -1,12 +1,9 @@
 ﻿using AutoMapper;
-using System;
-using MediatR;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using System.Text;
-using Notes.Application.Notes.Queries.GetNoteList;
-using Notes.Application.Notes.Queries.GetNoteDetails;
 using Notes.Application.Notes.Commands.CreateNote;
+using Notes.Application.Notes.Commands.UpdateNote;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+using Notes.Application.Notes.Queries.GetNoteList;
 using Notes.WebApi.Models;
 
 namespace Notes.WebApi.Controllers
@@ -15,7 +12,8 @@ namespace Notes.WebApi.Controllers
     {
         private readonly IMapper _mapper;
 
-        public NoteController(IMapper mapper)=>_mapper=mapper;
+        public NoteController(IMapper mapper) => _mapper = mapper;
+
         [HttpGet]
         public async Task<ActionResult<NoteListVm>> GetAll()
         {
@@ -27,6 +25,7 @@ namespace Notes.WebApi.Controllers
             var vm = await Mediator.Send(query);
             return Ok(vm);
         }
+
         [HttpGet("{id}")]
         public async Task<ActionResult<NoteDetailsVm>> Get(Guid id)
         {
@@ -38,13 +37,23 @@ namespace Notes.WebApi.Controllers
             var vm = await Mediator.Send(query);
             return Ok(vm);
         }
+
         [HttpPost]
         public async Task<ActionResult<Guid>> Create([FromBody] CreateNoteDto createNoteDto)
         {
             var command = _mapper.Map<CreateNoteCommand>(createNoteDto);
             command.UserId = UserId;
-            var noteId=await Mediator.Send(command);
+            var noteId = await Mediator.Send(command);
             return Ok(noteId);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateNoteDto updateNoteDto)
+        {
+            var command = _mapper.Map<UpdateNoteCommand>(updateNoteDto);
+            command.UserId = UserId;
+            await Mediator.Send(command);
+            return NoContent();
         }
     }
 }
