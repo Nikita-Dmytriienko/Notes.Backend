83dd9df521001d54824071ed4fa5a589192291db
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-12-20 13:32:16 +0200

    feat: add CreateNoteCommandValidator with FluentValidation rules

---
+++ Notes.Application/Notes/Commands/CreateNote/CreateNoteCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Notes.Application.Notes.Commands.CreateNote
+{   
+    public class CreateNoteCommandValidator : AbstractValidator<CreateNoteCommand>
+    {
+        public CreateNoteCommandValidator() 
+        {
+            RuleFor(CreateNoteCommand =>
+            CreateNoteCommand.Title).NotEmpty().MaximumLength(255);
+            RuleFor(CreateNoteCommand =>
+            CreateNoteCommand.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
