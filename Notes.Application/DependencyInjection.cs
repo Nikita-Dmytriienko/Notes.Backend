50b9a34ed8ff25e84e2c70b9430127141bd2880d
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-12-24 13:07:42 +0200

    feat: add DependencyInjection for FluentValidation, and ValidationBehavior

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -8,14 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MediatR;
-
+using FluentValidation;
+using Notes.Application.Common.Behaviors;
 namespace Notes.Application
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services
+                .AddMediatR(Assembly.GetExecutingAssembly());
+            services
+                .AddValidatorsFromAssemblies(new[] {Assembly.GetExecutingAssembly()});
+            services
+                .AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
             return services;
         }
     }
